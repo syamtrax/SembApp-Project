57ccfc1891c28ee75263522b3d8109b6e6e7eab2
hadidwirsty <m.hadid.2001@mail.ugm.ac.id>
2022-10-24 15:06:41 +0700

    Menambah Halaman Daftar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Daftar from "./pages/daftar";
 import Dashboard from "./pages/dashboard.js";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/daftar" element={<Daftar />} />
       </Routes>
     </BrowserRouter>
   );
