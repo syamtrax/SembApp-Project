a3013dd58fed86c9c3d604e54ea79c9177559449
ryannovianno <ryannovianno@users.noreply.github.com>
2022-10-24 20:29:22 +0700

    menambah halaman login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css'
 import Dashboard from "./pages/dashboard.js";
+import Login from "./pages/login.js";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login/>}/>
         <Route path="/" element={<Dashboard/>}/>
       </Routes>
     </BrowserRouter>
