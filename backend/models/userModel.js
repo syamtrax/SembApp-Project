4162b636b1de4dd06e788430ef3680214f93bfa1
Satria Syammahestatma <satriasyamm@gmail.com>
2022-10-24 14:50:17 +0700

    tambah usermodel

---
+++ backend/models/userModel.js
@@ -0,0 +1,21 @@
+import {Sequelize} from "sequelize";
+import db from "../config/database.js";
+
+const {DataTypes} = Sequelize;
+
+const User = db.define('user',{
+    namaToko: DataTypes.STRING,
+    alamatToko: DataTypes.STRING,
+    namaPengguna: DataTypes.STRING,
+    sandi : DataTypes.STRING,
+    email : DataTypes.STRING,
+    telp: DataTypes.STRING,
+    img : DataTypes.BLOB
+},{
+    freezeTableName:true
+});
+export default User;
+
+(async()=>{
+    await db.sync();
+})();
--- backend/routes/Route.js
+++ backend/routes/Route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getTransaction, getTransactionByID, createTransaction, updateTransaction, deleteTransaction, totalTransaction,sumTransaction } from "../controllers/transactionController.js";
+import { createUser, getUser, getUserByID } from "../controllers/userController.js";
 
 const router = express.Router();
 
@@ -10,6 +11,10 @@ router.patch('/transaction/:id', updateTransaction);
 router.delete('/transaction/:id', deleteTransaction);
 router.get('/total',totalTransaction); 
 router.get('/totalprice',sumTransaction);
+
+router.get('/user', getUser);
+router.get('/user/:id', getUserByID);
+router.post('/user', createUser);
 
 
 export default router;
