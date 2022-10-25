7ffea1420f6513a61f0f674e665a00ad26c818b5
hadidwirsty <m.hadid.2001@mail.ugm.ac.id>
2022-10-25 07:43:00 +0700

    clean code

--- frontend/src/pages/daftar.js
+++ frontend/src/pages/daftar.js
@@ -3,7 +3,7 @@ import backgroundDaftar from "../assets/Background Halaman Daftar.png";
 import { Link } from "react-router-dom";
 import { HiArrowLeft } from "react-icons/hi";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Daftar() {
   const [namaPengguna, setUsername] = useState("");
@@ -21,7 +21,7 @@ function Daftar() {
         sandi,
         telp,
         namaToko,
-        alamatToko
+        alamatToko,
       });
       navigate("/login");
     } catch (error) {
@@ -47,106 +47,106 @@ function Daftar() {
               Daftar Toko
             </h1>
             <form onSubmit={saveUser}>
-            <div className="flex flex-col">
               <div className="flex flex-col">
-                <label
-                  className="text-base text-gray-800"
-                  htmlFor="Nama Pengguna"
-                >
-                  Nama Pengguna
-                </label>
-                <input
-                  className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
-                  id="username"
-                  type="text"
-                  value = {namaPengguna}
-                  onChange={(e) => setUsername(e.target.value)}
-                  name="username"
-                  placeholder="Masukkan Nama Pengguna"
-                />
-                {/* {checkedUsername.length > 0 && (
+                <div className="flex flex-col">
+                  <label
+                    className="text-base text-gray-800"
+                    htmlFor="Nama Pengguna"
+                  >
+                    Nama Pengguna
+                  </label>
+                  <input
+                    className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
+                    id="username"
+                    type="text"
+                    value={namaPengguna}
+                    onChange={(e) => setUsername(e.target.value)}
+                    name="username"
+                    placeholder="Masukkan Nama Pengguna"
+                  />
+                  {/* {checkedUsername.length > 0 && (
                   <p className="text-red-500">{checkedUsername}</p>
                 )} */}
+                </div>
+                <div className="relative flex flex-col py-2">
+                  <label className="text-base" htmlFor="Kata Sandi">
+                    Kata Sandi
+                  </label>
+                  <div className="flex items-center justify-evenly">
+                    <input
+                      className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
+                      id="password"
+                      value={sandi}
+                      onChange={(e) => setPassword(e.target.value)}
+                      type="text"
+                      name="password"
+                      placeholder="Masukkan Kata Sandi"
+                    />
+                  </div>
+                  <div className="flex flex-col py-2">
+                    <label className="text-base" htmlFor="Nama Toko">
+                      Nama Toko
+                    </label>
+                    <input
+                      className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
+                      id="toko"
+                      type="text"
+                      value={namaToko}
+                      onChange={(e) => setNamatoko(e.target.value)}
+                      name="toko"
+                      placeholder="Masukkan Nama Toko"
+                    />
+                  </div>
+                  <div className="flex flex-col py-2">
+                    <label className="text-base" htmlFor="Alamat Toko">
+                      Alamat Toko
+                    </label>
+                    <input
+                      className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
+                      id="alamat"
+                      type="text"
+                      value={alamatToko}
+                      onChange={(e) => setAlamat(e.target.value)}
+                      name="alamat"
+                      placeholder="Masukkan Alamat Toko"
+                    />
+                  </div>
+                  <div className="flex flex-col py-2">
+                    <label className="text-base" htmlFor="Nomor Telepon">
+                      No.Telepon
+                    </label>
+                    <input
+                      className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
+                      id="nomor"
+                      type="number"
+                      value={telp}
+                      onChange={(e) => setNotelp(e.target.value)}
+                      name="nomor"
+                      placeholder="Masukkan No.Telepon"
+                    />
+                  </div>
+                  <div className="flex items-center text-sm">
+                    <input
+                      type="checkbox"
+                      className="w-4 h-4 border rounded text-black focus:border-black"
+                    />
+                    <span className="ml-2">
+                      Saya setuju dengan{" "}
+                      <a className="text-birumuda font-medium">
+                        Syarat dan Ketentuan
+                      </a>
+                    </span>
+                  </div>
+                  <div className="flex items-center flex-col">
+                    <button
+                      type="submit"
+                      className="bg-birumuda text-white px-4 py-3 mt-8 mb-4 text-base font-bold w-full radius rounded-full"
+                    >
+                      Daftar
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className="relative flex flex-col py-2">
-                <label className="text-base" htmlFor="Kata Sandi">
-                  Kata Sandi
-                </label>
-                <div className="flex items-center justify-evenly">
-                  <input
-                    className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
-                    id="password"
-                    value = {sandi}
-                    onChange={(e) => setPassword(e.target.value)}
-                    type="text"
-                    name="password"
-                    placeholder="Masukkan Kata Sandi"
-                  />
-                </div>
-                <div className="flex flex-col py-2">
-                  <label className="text-base" htmlFor="Nama Toko">
-                    Nama Toko
-                  </label>
-                  <input
-                    className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
-                    id="toko"
-                    type="text"
-                    value = {namaToko}
-                    onChange={(e) => setNamatoko(e.target.value)}
-                    name="toko"
-                    placeholder="Masukkan Nama Toko"
-                  />
-                </div>
-                <div className="flex flex-col py-2">
-                  <label className="text-base" htmlFor="Alamat Toko">
-                    Alamat Toko
-                  </label>
-                  <input
-                    className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
-                    id="alamat"
-                    type="text"
-                    value={alamatToko}
-                    onChange={(e) => setAlamat(e.target.value)}
-                    name="alamat"
-                    placeholder="Masukkan Alamat Toko"
-                  />
-                </div>
-                <div className="flex flex-col py-2">
-                  <label className="text-base" htmlFor="Nomor Telepon">
-                    No.Telepon
-                  </label>
-                  <input
-                    className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
-                    id="nomor"
-                    type="number"
-                    value={telp}
-                    onChange={(e) => setNotelp(e.target.value)}
-                    name="nomor"
-                    placeholder="Masukkan No.Telepon"
-                  />
-                </div>
-                <div className="flex items-center text-sm">
-                  <input
-                    type="checkbox"
-                    className="w-4 h-4 border rounded text-black focus:border-black"
-                  />
-                  <span className="ml-2">
-                    Saya setuju dengan{" "}
-                    <a className="text-birumuda font-medium">
-                      Syarat dan Ketentuan
-                    </a>
-                  </span>
-                </div>
-                <div className="flex items-center flex-col">
-                  <button
-                    type="submit"
-                    className="bg-birumuda text-white px-4 py-3 mt-8 mb-4 text-base font-bold w-full radius rounded-full"
-                  >
-                    Daftar
-                  </button>
-                </div>
-              </div>
-            </div>
             </form>
           </div>
         </div>
