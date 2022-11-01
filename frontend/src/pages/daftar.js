c60072862f65ae2847f7bac51030f54eb706e031
hadidwirsty <m.hadid.2001@mail.ugm.ac.id>
2022-11-01 07:42:27 +0700

    Update daftar

--- frontend/src/pages/daftar.js
+++ frontend/src/pages/daftar.js
@@ -1,28 +1,44 @@
 import React, { useState } from "react";
 import backgroundDaftar from "../assets/Background Halaman Daftar.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { HiArrowLeft } from "react-icons/hi";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 function Daftar() {
-  const [namaPengguna, setUsername] = useState("");
-  const [sandi, setPassword] = useState("");
-  const [telp, setNotelp] = useState("");
-  const [namaToko, setNamatoko] = useState("");
-  const [alamatToko, setAlamat] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [number, setNumber] = useState("");
+  const [store, setStore] = useState("");
+  const [address, setAddress] = useState("");
+  const [userError, setuserError] = useState("");
+  const [passError, setpassError] = useState("");
+  const [numError, setnumError] = useState("");
+  const [storError, setstorError] = useState("");
+  const [addError, setaddError] = useState("");
   const navigate = useNavigate();
   const [msg, setMsg] = useState("");
+
+  const validate = () => {
+    const errors = {};
+
+    if (username === "") errors.username = "Nama pengguna diperlukan";
+    if (password === "") errors.password = "Kata sandi diperlukan";
+    if (number === "") errors.number = "No.telepon diperlukan";
+    if (store === "") errors.store = "Nama toko diperlukan";
+    if (address === "") errors.address = "Alamat diperlukan";
+
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
 
   const saveUser = async (e) => {
     e.preventDefault();
     try {
       await axios.post("http://localhost:5000/user", {
-        namaPengguna,
-        sandi,
-        telp,
-        namaToko,
-        alamatToko,
+        username,
+        password,
+        number,
+        store,
+        address,
       });
       navigate("/");
     } catch (error) {
@@ -30,25 +46,45 @@ function Daftar() {
         setMsg(error.response.data.msg);
       }
     }
+
+    const errors = validate();
+    if (errors) {
+      setuserError(errors.username);
+      setPassword(errors.password);
+      setnumError(errors.number);
+      setstorError(errors.store);
+      setaddError(errors.address);
+    } else {
+      setuserError("");
+      setpassError("");
+      setnumError("");
+      setstorError("");
+      setaddError("");
+    }
   };
+
   return (
     <div className="grid grid-cols-2">
-      <img src={backgroundDaftar} className="h-screen w-screen"></img>
+      <div>
+        <img src={backgroundDaftar} className="h-screen w-screen"></img>
+      </div>
       <div className="bg-abumuda h-screen">
-        <div className="bg-white mx-36 my-24 rounded-3xl p-6">
-          <div>
-            <button className="font-medium flex items-center text-birumuda mt-4">
-              <Link to="/">
-                <HiArrowLeft
-                  // onClick={() => toggleOtp(false)}
-                  className=""
-                />
-              </Link>
-              Kembali
-            </button>
-            <h1 className="font-bold flex flex-col items-center text-5xl mt-1 mb-6">
-              Daftar Toko
-            </h1>
+        <div className="relative bg-white w-2/3 p-6 m-auto my-20 rounded-2xl drop-shadow-2xl">
+          <div className="flex flex-col">
+            <Link to="/">
+              <button className="flex mt-1 mb-2 font-medium items-center text-birumuda hover:underline">
+                <HiArrowLeft />
+                Kembali
+              </button>
+            </Link>
+            <h1 className="font-bold text-2xl mt-1 text-center">Daftar Toko</h1>
+            <p className="text-sm text-center text-gray-800 mb-6">
+              {" "}
+              Sudah memiliki Akun?{" "}
+              <a className="font-semibold text-birumuda hover:underline">
+                <Link to="/">Masuk</Link>
+              </a>
+            </p>
             <form onSubmit={saveUser}>
               <div className="flex flex-col">
                 <div className="flex flex-col">
@@ -62,82 +98,83 @@ function Daftar() {
                     <p className="text-base text-red-500">{msg}</p>
                   </div>
                   <input
-                    className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
+                    className="border rounded-lg w-full p-2 text-gray-900 bg-white focus:border-black"
                     id="username"
                     type="text"
-                    value={namaPengguna}
+                    value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     name="username"
                     placeholder="Masukkan Nama Pengguna"
                   />
-                  {/* {checkedUsername.length > 0 && (
-                  <p className="text-red-500">{checkedUsername}</p>
-                )} */}
+                  <div className="text-red-500 text-sm">{userError}</div>
                 </div>
-                <div className="relative flex flex-col py-2">
+                <div className="flex flex-col">
                   <label className="text-base" htmlFor="Kata Sandi">
                     Kata Sandi
                   </label>
-
                   <div className="flex items-center justify-evenly">
                     <input
-                      className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
+                      className="border rounded-lg w-full p-2 text-gray-900 bg-white focus:border-black"
                       id="password"
-                      value={sandi}
+                      value={password}
                       onChange={(e) => setPassword(e.target.value)}
                       type="password"
                       name="password"
                       placeholder="Masukkan Kata Sandi"
                     />
+                    <div className="text-red-500 text-sm">{password}</div>
                   </div>
-                  <div className="flex flex-col py-2">
+                  <div className="flex flex-col">
                     <label className="text-base" htmlFor="Nama Toko">
                       Nama Toko
                     </label>
                     <input
-                      className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
+                      className="border rounded-lg w-full p-2 text-gray-900 bg-white focus:border-black"
                       id="toko"
                       type="text"
-                      value={namaToko}
-                      onChange={(e) => setNamatoko(e.target.value)}
+                      value={store}
+                      onChange={(e) => setStore(e.target.value)}
                       name="toko"
                       placeholder="Masukkan Nama Toko"
                     />
+                    <div className="text-red-500 text-sm">{storError}</div>
                   </div>
-                  <div className="flex flex-col py-2">
+                  <div className="flex flex-col">
                     <label className="text-base" htmlFor="Alamat Toko">
                       Alamat Toko
                     </label>
                     <input
-                      className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
+                      className="border rounded-lg w-full p-2 text-gray-900 bg-white focus:border-black"
                       id="alamat"
                       type="text"
-                      value={alamatToko}
-                      onChange={(e) => setAlamat(e.target.value)}
+                      value={address}
+                      onChange={(e) => setAddress(e.target.value)}
                       name="alamat"
                       placeholder="Masukkan Alamat Toko"
                     />
+                    <div className="text-red-500 text-sm">{addError}</div>
                   </div>
-                  <div className="flex flex-col py-2">
+                  <div className="flex flex-col mb-3">
                     <label className="text-base" htmlFor="Nomor Telepon">
                       No.Telepon
                     </label>
                     <input
-                      className="border rounded-lg w-full p-3 mb-1 text-gray-900 bg-white focus:border-black"
+                      className="border rounded-lg w-full p-2 text-gray-900 bg-white focus:border-black"
                       id="nomor"
                       type="number"
-                      value={telp}
-                      onChange={(e) => setNotelp(e.target.value)}
+                      value={number}
+                      onChange={(e) => setNumber(e.target.value)}
                       name="nomor"
                       placeholder="Masukkan No.Telepon"
                     />
+                    <div className="text-red-500 text-sm">{numError}</div>
                   </div>
-                  <div className="flex items-center text-sm">
+                  <div className="flex items-center text-sm mt-1 mb-6">
                     <input
                       type="checkbox"
-                      className="w-4 h-4 border rounded text-black focus:border-black"
+                      className="w-4 h-4 border rounded focus:accent-birumuda"
                     />
-                    <span className="ml-2">
+                    <span className="ml-1">
                       Saya setuju dengan{" "}
                       <a className="text-birumuda font-medium">
                         Syarat dan Ketentuan
@@ -147,7 +184,7 @@ function Daftar() {
                   <div className="flex items-center flex-col">
                     <button
                       type="submit"
-                      className="bg-birumuda text-white px-4 py-3 mt-8 mb-4 text-base font-bold w-full radius rounded-full"
+                      className="bg-birumuda text-white py-3 mb-1 text-base font-bold w-full radius rounded-full"
                     >
                       Daftar
                     </button>
@@ -161,4 +198,5 @@ function Daftar() {
     </div>
   );
 }
+
 export default Daftar;
